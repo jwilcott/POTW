7b1a430514f1ecc5279fcf68a7fcfd4b4bbb32b7
Justin <jwilcott@gmail.com>
2025-01-12 21:29:32 -0800

    test

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 // ...existing code...
 
+let count = 0;
+
+function updateCountDisplay() {
+    console.log(`Current count: ${count}`);
+}
+
+function countUp() {
+    count++;
+    updateCountDisplay();
+}
+
+function countDown() {
+    count--;
+    updateCountDisplay();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('count-up').addEventListener('click', countUp);
+    document.getElementById('count-down').addEventListener('click', countDown);
+    updateCountDisplay();
+});
+
 function test() {
     console.log("The script is working!!");
 }
